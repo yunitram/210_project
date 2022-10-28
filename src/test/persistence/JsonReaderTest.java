c719bd04381d1c6112acede06e64b37bad0bb721
yunitram <martin7uy@gmail.com>
2022-10-28 00:10:39 -0700

    Phase 2 - added user stories to include save/load functionality

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,5 +1,6 @@
 package persistence;
 
+import model.CharacterList;
 import model.Tier;
 import model.TierList;
 import org.junit.jupiter.api.Test;
@@ -42,8 +43,12 @@ class JsonReaderTest extends JsonTest {
     @Test
     void testReaderGeneralTierList() {
         JsonReader reader = new JsonReader("./data/testReaderGeneralTierList.json");
+        CharacterList cl = new CharacterList();
+        cl.createCharacters();
+        assertEquals(5, cl.getCharacters().size());
         try {
             TierList tl = reader.read();
+            cl = reader.find();
             List<Tier> tiers = tl.getTiers();
             assertEquals("Tierlist 1", tl.getName());
             assertEquals(0, tiers.get(0).getCharacters().size());
@@ -53,6 +58,10 @@ class JsonReaderTest extends JsonTest {
             assertEquals(1, tiers.get(3).getCharacters().size());
             assertEquals("makima", tiers.get(3).getCharacters().get(0).getCharacterName());
             assertEquals(0, tiers.get(4).getCharacters().size());
+            assertEquals(2, cl.getCharacters().size());
+            assertEquals("goku", cl.getCharacters().get(0).getCharacterName());
+            assertEquals("makima", cl.getCharacters().get(1).getCharacterName());
+
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
